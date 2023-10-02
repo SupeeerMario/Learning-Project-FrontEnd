562492531c1d1c700397122b5084759b4b3334d7
SupeeerMario <khaledshaaban926@gmail.com>
2023-10-02 18:11:10 +0300

    good for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import first from './images/first.jpg';
 import './App.css';
+
+import first from './images/first.jpg';
+import sec from './images/sec.JPG';
+import third from './images/third.jpg';
+import forth from './images/forth.jpg';
+
+
+const imagelst = [first,sec,third,forth];
 
 function App() {
   const [message, setMessage] = useState('');
+  const [displayedImage, setDisplayedImage] = useState(null);
+
 
   const handleChange = (event) => {
     setMessage(event.target.value);
@@ -19,6 +28,11 @@ function App() {
     const desiredWord = 'khaled';
     if(message.includes(desiredWord)){
       console.log("Done");
+      const randomIndex = Math.floor(Math.random() * imagelst.length);
+      const randomImage = imagelst[randomIndex];
+      setDisplayedImage(randomImage);
+   
+
     }
     else{
       console.log("lsa");
@@ -29,7 +43,9 @@ function App() {
   return (
     <div className="App">
       <div className="imgdiv">
-        <img src={first} alt="image" />
+      <img src={displayedImage} alt="displayed-image" />
+
+
 
         <div className="nputdev">
           <input onChange={handleChange} className="nput" type="text" placeholder="Enter a name" />
